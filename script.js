067e1f1de51f8bd09dba605b91ad99f15602ff3f
Putra Wardoyo <melsaputraw@gmail.com>
2025-05-17 14:54:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,6 @@ loginButton.addEventListener('click', function() {
     const validCredentials = [
         { username: 'admin', password: 'admin123' },
         { username: 'user1', password: 'user1234' },
-        { username: 'user2', password: 'password567' },
         { username: 'putra', password: 'putraward25' }
     ];
 
